e14fd96dc1fdcfe5a3b2029b77c5a5354060a423
Avni Alp Mumcu <sysreqpro@gmail.com>
2015-10-19 22:33:07 -0400

    Creating a new github application named ror1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ROR1::Application.config.secret_token = '608009a4290e19c8c64ceb7fc6bc2f84493d494687883c3f55d83fdae6eb80213c1a078be05df477e273eeff86c81115bb58b6d96af305628b7f9ac84460a6a4'
